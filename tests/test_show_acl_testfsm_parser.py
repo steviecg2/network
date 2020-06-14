8bce25a7e4165a581e547d5eb1d32853b24363f8
Stephen Guilfoil <sguilfoil1@gmail.com>
2020-06-14 18:40:10 +0000

    adding configuration of acls to repo, not done yet

--- tests/test_show_acl_testfsm_parser.py
+++ tests/test_show_acl_testfsm_parser.py
@@ -1,20 +1,47 @@
+import json
+import os
 import textfsm
 
-show_access_list = """
-Extended IP access list outside_acl
-    10 permit icmp any any (2680779 matches)
-    20 permit udp any eq domain any (88 matches)
-    30 permit tcp any eq domain any (1445 matches)
-    40 permit udp any eq bootps any (1268 matches)
-    50 permit udp any any eq isakmp (5165 matches)
-    60 permit udp any any eq non500-isakmp (6360519 matches)
-    70 permit udp any eq non500-isakmp any
-    80 permit tcp any any eq 8443 (903 matches)
-    90 permit tcp any any eq 443 (267211 matches)
-    100 permit udp any any eq 443 (144 matches)
-    110 permit udp any any eq 8443
-    120 permit tcp any any range 8000 8020 (1024900 matches)
-    130 permit tcp any any established (23275 matches)
+directory = os.path.dirname(os.path.realpath(__file__))
+template = open(f"{directory}/../parsers/cisco_ios_show_access-list.template")
+output = 
 """
+Extended IP access list auto_test
+    10 deny ip host 1.1.1.1 host 1.1.1.2 log
+    20 deny tcp any eq telnet 1.2.3.0 0.0.0.255 range 555 560
+    30 permit tcp 5.5.5.0 0.0.0.255 gt 12345 10.0.0.0 0.255.255.255 eq smtp established
+    40 deny udp host 4.4.4.4 any lt 5000 log-input
+    50 deny icmp any any mask-reply
+    60 permit tcp object-group test_obj range 40 www 10.0.0.0 0.255.255.255 eq www pop2 pop3
+"""
+current = 
+"""
+['NAME', 'SN', 'ACTION', 'PROTOCOL', 'SOURCE', 'PORT', 'RANGE', 'DESTINATION', 'MODIFIER']
+['auto_test', '10', 'deny', 'ip', 'host 1.1.1.1', '', '', 'host 1.1.1.2', 'log']
+['auto_test', '20', 'deny', 'tcp', 'any', 'eq', 'telnet', '1.2.3.0 0.0.0.255', 'range 555 560']
+['auto_test', '30', 'permit', 'tcp', '5.5.5.0 0.0.0.255', 'gt', '12345', '10.0.0.0 0.255.255.255', 'eq smtp established']
+['auto_test', '40', 'deny', 'udp', 'host 4.4.4.4', '', '', 'any', 'lt 5000 log-input']
+['auto_test', '50', 'deny', 'icmp', 'any', '', '', 'any', 'mask-reply']
+"""
+expected = 
+"""
+['NAME',        'SN', 'ACTION', 'PROTOCOL', 'SOURCE',               'PORT', 'RANGE',    'DESTINATION',              'MODIFIER']
+['auto_test',   '10', 'deny',   'ip',       'host 1.1.1.1',         '',     '',         'host 1.1.1.2',             'log']
+['auto_test',   '20', 'deny',   'tcp',      'any',                  'eq',   'telnet',   '1.2.3.0 0.0.0.255',        'range 555 560']
+['auto_test',   '30', 'permit', 'tcp',      '5.5.5.0 0.0.0.255',    'gt',   '12345',    '10.0.0.0 0.255.255.255',   'eq smtp established']
+['auto_test',   '40', 'deny',   'udp',      'host 4.4.4.4',         '',     '',         'any',                      'lt 5000 log-input']
+['auto_test',   '50', 'deny',   'icmp',     'any',                  '',     '',         'any',                      'mask-reply']
+"""
+# port should be source_operator
+# sn should be sequence_number
+# range should be source_port
+# modified needs additional logic
+#   elimiinate matches
+#   derive a destination operator
+#   derive a destination port
+#   derive l4_established
+#   derive logging
 
-
+template = textfsm.TextFSM(template)
+output = template.ParseText(output)
+print(json.dumps(output, indent=4))
